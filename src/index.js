fb96d5ece2f9a8c600bbd737e16974521ceae566
ali081809 <1330510189@qq.com>
2020-08-06 19:04:02 +0800

    0806项目搭建完成

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+// 引入重置样式
+import "./assets/css/reset.css"
+// 引入rem.js
+import "./assets/js/rem"
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
